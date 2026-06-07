48c1fcd76de3debbdccf9ff9a5c6e92976c1be6e
wasp-salander <fritu7@gmail.com>
2026-06-07 12:04:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+const clickSound = new Audio("sound_pen_click.wav");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+   button.addEventListener('click', () => {
+      clickSound.currentTime = 0;
+      clickSound.play();
+   });
+});
